c19c8514bee32b44ec2727f02199beee9657481a
Guilherme Alves <guigui21alves@gmail.com>
2024-09-04 19:46:33 -0300

    [feat] Inclusão System.out.println

--- FazerVsCode/src/ra_2920482411018.java
+++ FazerVsCode/src/ra_2920482411018.java
@@ -1,7 +1,17 @@
 public class Ra_2920482411018 {
-  String nomeCompleto = "Guilherme Alves de Campos";
-  String nomeCompleto = "Guilherme Alves de Campos";
-  String nomeCompleto = "Guilherme Alves de Campos";
-  String nomeCompleto = "Guilherme Alves de Campos";
-  String nomeCompleto = "Guilherme Alves de Campos";
+  public static void main(String[] args) {
+    String nomeCompleto = "Guilherme Alves de Campos";
+    String ra = "2920482411018";
+    String idade = "18 anos";
+    String github = "@CampsGui";
+    String linkedin = "@guilherme-alves-de-campos";
+    String cursoTurma = "ADS 2º Semestre";
+  
+    System.out.println(nomeCompleto);
+    System.out.println(ra);
+    System.out.println(idade);
+    System.out.println(github);
+    System.out.println(linkedin);
+    System.out.println(cursoTurma);
+  }
 }
