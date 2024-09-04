a615bf9dd419d6f4330c876c322e36f9d627ba14
Guilherme Alves <guigui21alves@gmail.com>
2024-09-04 18:40:57 -0300

    [feat] Inclusão Variaveis

--- FazerVsCode/src/ra_2920482411018.java
+++ FazerVsCode/src/ra_2920482411018.java
@@ -1,3 +1,7 @@
-public class ra_2920482411018 {
-  String Nome_completo = "Guilherme Alves de Campos";
+public class Ra_2920482411018 {
+  String nomeCompleto = "Guilherme Alves de Campos";
+  String nomeCompleto = "Guilherme Alves de Campos";
+  String nomeCompleto = "Guilherme Alves de Campos";
+  String nomeCompleto = "Guilherme Alves de Campos";
+  String nomeCompleto = "Guilherme Alves de Campos";
 }
